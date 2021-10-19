c954d0dfda5062de109f6c0623593e6b1afc30c3
geoffrey <geoffrey.maillot@gmail.com>
2021-10-19 11:03:45 +0200

    Vider les champs du formalaire à l'envoie

--- js/contact-formulaire.js
+++ js/contact-formulaire.js
@@ -5,26 +5,31 @@ const contactForm = {
     contactForm.formEl = document.getElementById('contact_form')
     contactForm.formEl.addEventListener('submit', contactForm.handleSendMessage)
   },
-  handleSendMessage: async () => {
+  handleSendMessage: async (event) => {
     event.preventDefault();
-    const inputName = document.getElementById('form_name').value
-    const inputEmail = document.getElementById('form_email').value
-    const inputSubject = document.getElementById('form_subject').value
-    const inputMessage = document.getElementById('form_message').value
+    const inputName = document.getElementById('form_name')
+    const inputEmail = document.getElementById('form_email')
+    const inputSubject = document.getElementById('form_subject')
+    const inputMessage = document.getElementById('form_message')
 
     try {
 
-      if (!inputName.trim() || !inputEmail.trim() || !inputSubject.trim() || !inputMessage.trim()) {
+      if (!inputName.value.trim() || !inputEmail.value.trim() || !inputSubject.value.trim() || !inputMessage.value.trim()) {
         return
       } else {
         await emailjs.send("service_hrk7k9m", "template_n87n47f", {
-          name: inputName,
-          email: inputEmail,
-          subject: inputSubject,
-          message: inputMessage,
+          name: inputName.value,
+          email: inputEmail.value,
+          subject: inputSubject.value,
+          message: inputMessage.value,
         });
 
         alert('Votre Message a bien été transmis')
+        inputName.value = '';
+        inputEmail.value = '';
+        inputSubject.value = '';
+        inputMessage.value = '';
+
       }
 
 
